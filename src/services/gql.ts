6c69e6fbfd7f6f12387239aa2c7deaec66f25721
sanjaymalik1 <msanjay31103@gmail.com>
2025-08-22 01:41:34 +0530

    changes in environment variables

--- src/services/gql.ts
+++ src/services/gql.ts
@@ -1,5 +1,5 @@
 import {GraphQLClient} from "graphql-request"
 
-const gqlClient = new GraphQLClient("/api/graphql")
+const gqlClient = new GraphQLClient(`${process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT}/api/graphql`)
 
 export default gqlClient;
