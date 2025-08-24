1a09400106da1731d082a051aac358dd513f45f1
sanjaymalik1 <msanjay31103@gmail.com>
2025-08-24 16:48:58 +0530

    logn issue resolved

--- src/services/gql.ts
+++ src/services/gql.ts
@@ -1,9 +1,20 @@
-import {GraphQLClient} from "graphql-request"
+// import {GraphQLClient} from "graphql-request"
 
 
-// console.log("gql endpoint: ",process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT);
+// // console.log("gql endpoint: ",process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT);
 
 
-const gqlClient = new GraphQLClient(`/api/graphql`)
+// const gqlClient = new GraphQLClient(`/api/graphql`)
+
+// export default gqlClient;
+
+import { GraphQLClient } from "graphql-request";
+
+const endpoint =
+  typeof window === "undefined"
+    ? `${process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000"}/api/graphql`
+    : "/api/graphql";
+
+const gqlClient = new GraphQLClient(endpoint);
 
 export default gqlClient;
