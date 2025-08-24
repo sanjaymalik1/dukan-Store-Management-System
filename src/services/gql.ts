0c4c3907b7764f3b68dd0b931a0d85474e9e2cc9
sanjaymalik1 <msanjay31103@gmail.com>
2025-08-24 16:35:25 +0530

    relative url

--- src/services/gql.ts
+++ src/services/gql.ts
@@ -1,9 +1,9 @@
 import {GraphQLClient} from "graphql-request"
 
 
-console.log("gql endpoint: ",process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT);
+// console.log("gql endpoint: ",process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT);
 
 
-const gqlClient = new GraphQLClient(`${process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT}/api/graphql`)
+const gqlClient = new GraphQLClient(`/api/graphql`)
 
 export default gqlClient;
