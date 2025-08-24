fb8cea5b356c432af7ff5fcb8724d3a42f366ecd
sanjaymalik1 <msanjay31103@gmail.com>
2025-08-24 16:21:37 +0530

    debugging login issue

--- src/services/gql.ts
+++ src/services/gql.ts
@@ -1,5 +1,9 @@
 import {GraphQLClient} from "graphql-request"
 
+
+console.log("gql endpoint: ",process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT);
+
+
 const gqlClient = new GraphQLClient(`${process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT}/api/graphql`)
 
 export default gqlClient;
